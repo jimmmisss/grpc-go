02d5371d3da35d5ac089cf0ef59b5b8e6f1a2e85
Wesley Pereira <jimmmisss@gmail.com>
2022-01-30 13:55:12 -0300

    Implementando server

---
+++ server/main.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	user "go-grpc/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":8200")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := Server{}
+
+	grpcServer := grpc.NewServer()
+
+	user.RegisterUserServiceServer(grpcServer, &server)
+
+	log.Println("Listening on Port: 8200!")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+
+}
--- server/user.go
+++ server/user.go
@@ -1,9 +1,20 @@
 package server
 
+import (
+	"encoding/json"
+	"fmt"
+	user "go-grpc/proto"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/context"
+)
+
 type Server struct {
 }
 
-type user struct {
+type User struct {
 	ID         int64  `json:"id"`
 	Name       string `json:"name"`
 	Username   string `json:"username"`
@@ -11,9 +22,44 @@ type user struct {
 	Location   string `json:"location"`
 	Followers  int64  `json:"followers"`
 	Following  int64  `json:"following"`
-	Repos      string `json:"repos"`
-	Gists      string `json:"gists"`
+	Repos      int64  `json:"repos"`
+	Gists      int64  `json:"gists"`
 	URL        string `json:"URL"`
 	StarredURL string `json:"starredURL"`
 	ReposURL   string `json:"reposURL"`
+}
+
+func (s *Server) GetUser(ctx context.Context, in *user.UserRequest) (*user.UserResponse, error) {
+	log.Printf("Receive message from client: %s", in.Username)
+
+	res, err := http.Get(fmt.Sprintf("https://api.github.com/users/%v", in.Username))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	usr := User{}
+	jsonErr := json.Unmarshal(body, &usr)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return &user.UserResponse{
+		Id:        usr.ID,
+		Name:      usr.Name,
+		Username:  usr.Username,
+		Avatarurl: usr.AvatarURL,
+		Location:  usr.Location,
+		Statistics: &user.Statistics{
+			Followers: usr.Followers,
+			Following: usr.Following,
+			Repos:     usr.Repos,
+			Gists:     usr.Gists,
+		},
+		ListURLs: []string{usr.URL, usr.StarredURL, usr.ReposURL},
+	}, nil
 }
